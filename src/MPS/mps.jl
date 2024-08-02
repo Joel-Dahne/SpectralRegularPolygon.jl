d3918fa27028eaf4f5690e529fc00c88f9481b17
Joel Dahne <joel@dahne.eu>
2024-08-02 16:36:02 +0200

    mps: Add qr_eltype argument for use in sigma

--- src/MPS/mps.jl
+++ src/MPS/mps.jl
@@ -6,6 +6,7 @@
         N::Integer;
         num_boundary::Integer = 2N,
         num_interior::Integer = 2N,
+        qr_eltype = ifelse(T == Arb, BigFloat, T),
         verbose = false,
 )
 
@@ -18,10 +19,11 @@ function mps(
     N::Integer;
     num_boundary::Integer = 2N,
     num_interior::Integer = 2N,
+    qr_eltype = ifelse(T == Arb, BigFloat, T),
     verbose = false,
 ) where {T}
     res = Optim.optimize(a, b, show_trace = verbose) do λ
-        sigma(u, λ, N; num_boundary, num_interior)
+        sigma(u, λ, N; num_boundary, num_interior, qr_eltype)
     end
 
     Optim.converged(res) || @warn "Optimization failed"
@@ -36,6 +38,7 @@ end
         N::Integer;
         num_boundary::Integer = 2N,
         num_interior::Integer = 2N,
+        qr_eltype = ifelse(T == Arb, BigFloat, T),
         verbose = false,
 )
 
@@ -51,11 +54,12 @@ function mps(
     N::Integer;
     num_boundary::Integer = 2N,
     num_interior::Integer = 2N,
+    qr_eltype = ifelse(T == Arb, BigFloat, T),
     verbose = false,
 ) where {T}
     # TODO: This might not be a good idea
     res = Optim.optimize([λ₀], LBFGS()) do λ
-        sigma(u, only(λ), N; num_boundary, num_interior)
+        sigma(u, only(λ), N; num_boundary, num_interior, qr_eltype)
     end
 
     Optim.converged(res) || @warn "Optimization failed"
@@ -71,8 +75,9 @@ end
         N::Integer;
         num_boundary::Integer = 2N,
         num_interior::Integer = 2N,
+        qr_eltype = ifelse(T == Arb, BigFloat, T),
         verbose = false,
-)
+    )
 
 Like [`mps`](@ref) but also sets the coefficients of `u` to the ones
 corresponding to `λ`.
@@ -84,11 +89,12 @@ function mps!(
     N::Integer;
     num_boundary::Integer = 2N,
     num_interior::Integer = 2N,
+    qr_eltype = ifelse(T == Arb, BigFloat, T),
     verbose = false,
 ) where {T}
-    λ = mps(u, a, b, N; num_boundary, num_interior, verbose)
+    λ = mps(u, a, b, N; num_boundary, num_interior, qr_eltype, verbose)
 
-    sigma!(u, λ, N; num_boundary, num_interior)
+    sigma!(u, λ, N; num_boundary, num_interior, qr_eltype)
 
     return λ
 end
@@ -99,11 +105,12 @@ function mps!(
     N::Integer;
     num_boundary::Integer = 2N,
     num_interior::Integer = 2N,
+    qr_eltype = ifelse(T == Arb, BigFloat, T),
     verbose = false,
 ) where {T}
-    λ = mps(u, λ₀, N; num_boundary, num_interior, verbose)
+    λ = mps(u, λ₀, N; num_boundary, num_interior, qr_eltype, verbose)
 
-    sigma!(u, λ, N; num_boundary, num_interior)
+    sigma!(u, λ, N; num_boundary, num_interior, qr_eltype)
 
     return λ
 end
