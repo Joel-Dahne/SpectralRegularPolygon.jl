161cb7fbd93753d639c22f5c7f261be770bb0684
Joel Dahne <joel@dahne.eu>
2025-07-04 12:52:49 +0200

    indeterminate: Add method

--- src/arb.jl
+++ src/arb.jl
@@ -1,3 +1,27 @@
+# There is no version of this in Arblib
+function Arblib.indeterminate!(x::Union{ArbSeries,AcbSeries})
+    for i = 0:Arblib.degree(x)
+        Arblib.indeterminate!(Arblib.ref(x, i))
+    end
+    # Since we manually set the coefficients of the polynomial we
+    # need to also manually set the degree.
+    Arblib.cstruct(x).length = Arblib.degree(x) + 1
+    return x
+end
+
+"""
+    indeterminate(x)
+
+Construct an indeterminate version of `x`.
+"""
+indeterminate(x::Union{Arblib.ArbOrRef,Arblib.AcbOrRef}) = Arblib.indeterminate!(zero(x))
+indeterminate(::Type{T}) where {T<:Union{Arb,Acb}} = Arblib.indeterminate!(zero(T))
+indeterminate(x::Union{ArbSeries,AcbSeries}) = Arblib.indeterminate!(zero(x))
+indeterminate(::Type{T}) where {T<:AbstractFloat} = convert(T, NaN)
+indeterminate(::Type{Complex{T}}) where {T<:AbstractFloat} =
+    convert(Complex{T}, complex(NaN, NaN))
+indeterminate(x) = indeterminate(typeof(x))
+
 function Base.hypot(x::ArbSeries, y::ArbSeries)
     res = x^2
     Arblib.add_series!(res, res, y^2, length(res))
