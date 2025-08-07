33006a180d88f31762668fcf3bdfca8ca07d1308
Joel Dahne <joel@dahne.eu>
2025-08-07 15:31:36 -0500

    Section 2.2 Lemmas: Add skeleton

--- src/SpectralRegularPolygons.jl
+++ src/SpectralRegularPolygons.jl
@@ -40,5 +40,6 @@ include("large_N/TaylorModel.jl")
 include("large_N/polylog.jl")
 include("large_N/section_2.1.jl")
 include("large_N/section_2.1_lemmas.jl")
+include("large_N/section_2.2_lemmas.jl")
 
 end # module SpectralRegularPolygons
---
+++ src/large_N/section_2.2_lemmas.jl
@@ -0,0 +1,31 @@
+###
+# Lemma 2.15
+###
+
+function lemma_2_15(; verbose = true)
+
+end
+
+###
+# Lemma 2.16
+###
+
+function lemma_2_16(; verbose = true)
+
+end
+
+###
+# Lemma 2.19
+###
+
+function lemma_2_19(; verbose = true)
+
+end
+
+###
+# Lemma 2.20
+###
+
+function lemma_2_20(; verbose = true)
+
+end
